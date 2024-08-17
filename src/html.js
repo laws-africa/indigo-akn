8011656f8acb6f0184ff73daef923a0d10429990
goose-life <32566441+goose-life@users.noreply.github.com>
2024-08-17 10:16:33 +0200

    add fixTable to htmlToAkn for jS table editing

--- src/html.js
+++ src/html.js
@@ -89,6 +89,69 @@ export function htmlToAkn (html) {
     });
   }
 
+  function fixTable(table) {
+    let xmlns = table.getAttribute('xmlns'),
+        tableMap = mapTable(table),
+        nMappedRows = Object.keys(tableMap).length;
+
+    // add missing rows
+    let nMissingRows = nMappedRows - table.childNodes.length;
+    for (let y = 0; y < nMissingRows; y++) {
+      console.log("adding a missing row to table ", table.getAttribute('eId'));
+      table.appendChild(document.createElementNS(xmlns, 'tr'));
+    }
+
+    // add missing cells
+    let nMaxMappedColumns = Math.max(...Object.values(tableMap).map(r => Object.keys(r).length));
+    for (let r = 0; r < table.children.length; r++) {
+      let nMissingCells = nMaxMappedColumns - Object.keys(tableMap[r]).length,
+          row = table.children[r];
+      for (let m = 0; m < nMissingCells; m++) {
+        console.log("adding a missing cell to table ", table.getAttribute('eId'), " row ", r);
+        row.appendChild(document.createElementNS(xmlns, 'td'));
+      }
+    }
+
+    return table;
+  }
+
+  function mapTable(table) {
+    let matrix = {},
+        rows = table.children;
+
+    for (let y = 0; y < rows.length; y++) {
+      // set default
+      if (!matrix[y]) {
+        matrix[y] = {};
+      }
+      let row = rows[y];
+      let cells = row.children;
+      for (let x = 0; x < cells.length; x++) {
+        // stash 'x' so we don't end the loop prematurely if it's incremented below
+        let xPos = x;
+        let cell = cells[x];
+        // set default; increment xPos if needed (skip already occupied cells in current row)
+        while (matrix[y][xPos]) {
+          xPos += 1;
+        }
+        matrix[y][xPos] = {};
+        // mark matrix elements occupied by current cell with true
+        let colSpanTotal = xPos + Number(cell.getAttribute('colspan') || 1);
+        let rowSpanTotal = y + Number(cell.getAttribute('rowspan') || 1);
+
+        for (let xx = xPos; xx < colSpanTotal; xx++) {
+          for (let yy = y; yy < rowSpanTotal; yy++) {
+            if (!matrix[yy]) {
+              matrix[yy] = {};
+            }
+            matrix[yy][xx] = true;
+          }
+        }
+      }
+    }
+    return matrix;
+  }
+
   clean(html);
 
   // html -> xhtml
@@ -99,6 +162,12 @@ export function htmlToAkn (html) {
   if (!xml) {
     console.log("htmlTransform.transformToFragment returned null, input was: ", table.outerHTML);
     return null;
+  }
+
+  // add missing rows and cells to tables
+  // TODO: better check for this?
+  if (html.tagName === "TABLE") {
+    fixTable(xml.firstChild);
   }
 
   // strip whitespace at start of first p tag in table cells
